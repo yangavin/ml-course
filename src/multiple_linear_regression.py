b38a6c6de804b4ce09a448ae96d676393081325a
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-04-28 18:08:57 -0400

    add data and preprocess data for multiple regression

---
+++ src/multiple_linear_regression.py
@@ -0,0 +1,29 @@
+# import libraries
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+
+# Import data and split dependent and independent variables
+dataset = pd.read_csv("data/50_Startups.csv")
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Imput missing data
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+imputer.fit(x[:, :2])
+x[:, :2] = imputer.transform(x[:, :2])
+
+
+# One Hot Encode categorical independent variables
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(
+    transformers=[("encoder", OneHotEncoder(), [3])], remainder="passthrough"
+)
+x = np.array(ct.fit_transform(x))
+
+# Split train and test data
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
