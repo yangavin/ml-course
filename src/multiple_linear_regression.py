a08edefc4ffa3ea62b5aba3e750bde6f433c0642
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-04-28 18:49:52 -0400

    multiple regression predict

--- src/multiple_linear_regression.py
+++ src/multiple_linear_regression.py
@@ -27,3 +27,16 @@ x = np.array(ct.fit_transform(x))
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Train
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# Predict
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(
+    np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)
+)
